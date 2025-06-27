161a14c384459e7b286f7298803434fac9143549
jdencala <jdencalahuaya@gmail.com>
2025-06-27 18:44:59 -0500

    Add(I): Initial Commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function agendarLlamada() {
+    alert("Gracias por tu interés. Muy pronto nos pondremos en contacto contigo 😊");
+    // Puedes reemplazar esto con un link a Calendly, WhatsApp o un formulario
+    // window.location.href = "https://wa.me/tu_numero" o similar
+}
